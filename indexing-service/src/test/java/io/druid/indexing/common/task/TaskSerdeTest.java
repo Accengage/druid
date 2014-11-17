b72ad9e6bcbf8fbabbee1bf8af13e36904d4ef9f
Pierre-Edouard Montabrun <pemontabrun@accengage.com>
2014-11-17 18:31:54 +0100

    Testserde modified to use the new version of config properties.

--- indexing-service/src/test/java/io/druid/indexing/common/task/TaskSerdeTest.java
+++ indexing-service/src/test/java/io/druid/indexing/common/task/TaskSerdeTest.java
@@ -418,6 +418,7 @@ public class TaskSerdeTest
             false,
             ImmutableMap.of("foo", "bar"),
             false,
+            false,
             null,
             null,
             null,
